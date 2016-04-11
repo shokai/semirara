5f3c24f9ac6749cab55b122ce49ff85c6b66516e
Sho Hashimoto <hashimoto@shokai.org>
2016-04-11 13:08:02 +0900

    fix styles  #4

--- client/component/login.jsx
+++ client/component/login.jsx
@@ -13,29 +13,22 @@ export default class Login extends Component {
   }
 
   render(){
+    let element;
     if(!this.state.user){
-      return (
-        <div className="login">
-          <ul><li><span><a href="/auth/login">login</a></span></li></ul>
-        </div>
-      );
+      element = <ul><li><span><a href="/auth/login">login</a></span></li></ul>;
     }
     else{
-      return (
-        <div className="login">
-          <ul>
-            <li>
-              <span>
-                <a href={"https://github.com/"+this.state.user.name}>
-                  <img src={this.state.user.icon+"&s=20"} />
-                  {this.state.user.name}
-                </a>
-              </span>
-            </li>
-            <li><span><a href="/auth/logout">logout</a></span></li>
-          </ul>
-        </div>
+      element = (
+        <ul>
+          <li><span><a href={"https://github.com/"+this.state.user.name}>
+                <img src={this.state.user.icon+"&s=20"} />{this.state.user.name}
+          </a></span></li>
+          <li><span><a href="/auth/logout">logout</a></span></li>
+        </ul>
       );
     }
+    return (
+      <div className="login">{element}</div>
+    );
   }
 }
