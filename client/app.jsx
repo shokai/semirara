e27f4e8b353d3640bd203e7974ac2c0fe4553c11
Sho Hashimoto <hashimoto@shokai.org>
2016-04-12 23:25:02 +0900

    set max-width  #4

--- client/app.jsx
+++ client/app.jsx
@@ -11,7 +11,7 @@ export default class App extends Component{
   render(){
     debug("render()");
     return (
-      <div>
+      <div className="app">
         <Header />
         <Editor />
       </div>
