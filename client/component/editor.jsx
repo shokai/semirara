e573898c32d53b4f72611dd9c2b03122506401b3
Sho Hashimoto <hashimoto@shokai.org>
2016-04-15 18:55:44 +0900

    [WIP] edit line  #6

--- client/component/editor.jsx
+++ client/component/editor.jsx
@@ -8,25 +8,52 @@ export default class Editor extends Component {
   constructor(){
     super();
     this.onChange = this.onChange.bind(this);
+    this.editline = this.editline.bind(this);
   }
 
   mapState(state){
-    return {page: state.page, user: state.user};
+    return {page: state.page, user: state.user, editline: null};
   }
 
   render(){
     this.debug("render()");
-    let key = 0;
-    const lines = this.state.page.lines.map((line) => {
-      return <li key={key++}>{compile(line)}</li>;
-    });
+    let i = 0;
+    const lines = [];
+    for(let i = 0; i < this.state.page.lines.length; i++){
+      const line = this.state.page.lines[i];
+      const onClick = (e) => {
+        e.stopPropagation();
+        this.editline(i);
+      };
+      let li;
+      if(i === this.state.editline){
+        li = <li key={i}><input value={line} /></li>;
+      }
+      else{
+        li = <li key={i}><span onClick={onClick}>{compile(line)}</span></li>;
+      }
+      lines.push(li);
+    }
     return (
-      <div className="editor">
+      <div className="editor" onClick={this.editline}>
         <h1>editor</h1>
         <ul>{lines}</ul>
         <textarea onChange={this.onChange} value={this.state.page.lines.join("\n")} />
       </div>
     );
+  }
+
+  onLineClick(e){
+    this.debug(e.target);
+    const editline = e.target.attributes.num.value;
+    this.debug(editline);
+    this.setState({editline});
+  }
+
+  editline(editline){
+    if(typeof editline !== "number") editline = null;
+    this.debug(`set editline ${editline}`);
+    this.setState({editline});
   }
 
   onChange(e){
