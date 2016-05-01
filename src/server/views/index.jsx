e421bed8f53e85ee0c9ec7af20d7ff3377904ec1
Sho Hashimoto <hashimoto@shokai.org>
2016-05-01 18:57:08 +0900

    load react & react-dom from CDN  #44

--- src/server/views/index.jsx
+++ src/server/views/index.jsx
@@ -6,6 +6,13 @@ export default function IndexHTML({user, app}){
 
   const script = `window.user = ${JSON.stringify(user)}; window.app = ${JSON.stringify(app)};`;
 
+  const cdnjs = (
+    <x-cdnjs>
+      <script src="//cdnjs.cloudflare.com/ajax/libs/react/15.0.2/react.min.js" />
+      <script src="//cdnjs.cloudflare.com/ajax/libs/react/15.0.2/react-dom.min.js" />
+    </x-cdnjs>
+  );
+
   return (
     <html>
       <head>
@@ -16,6 +23,7 @@ export default function IndexHTML({user, app}){
       </head>
       <body>
         <div id="app" />
+        {process.env.NODE_ENV === "production" ? cdnjs : null}
         <script src="/dist/index.js" />
       </body>
     </html>
