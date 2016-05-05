26744dca2f2afd375a9b9e3695d7330a5e325d9a
Sho Hashimoto <hashimoto@shokai.org>
2016-05-05 16:19:12 +0900

    fixed JSX

--- src/server/views/index-static.jsx
+++ src/server/views/index-static.jsx
@@ -15,7 +15,7 @@ export default function IndexStaticHTML({user, app, state}){
         <meta name="viewport" content="width=640px" />
         <title>{buildTitle({wiki, title, lines})}</title>
         <link rel="stylesheet" href="/dist/index.css" />
-        <link rel="alternate" type="application/rss+xml" title="RSS Feed" href={`/api/feed/${wiki}`}/>
+        <link rel="alternate" type="application/rss+xml" title="RSS Feed" href={`/api/feed/${wiki}`} />
         <TwitterCard page={state.page} />
       </head>
       <body>
