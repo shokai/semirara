000999cda4e1a8471241be22aa5bce5a1552ccdf
Sho Hashimoto <hashimoto@shokai.org>
2016-05-30 00:22:53 +0900

    bugfix pagelist image css  #55

--- src/client/component/pagelist.jsx
+++ src/client/component/pagelist.jsx
@@ -17,7 +17,7 @@ export default class PageList extends StoreComponent {
     const {wiki} = this.state.page
     const list = this.state.pagelist.map(({title, image}) => {
       const style = image ? {
-        backgroundImage: `url("${image}"`
+        backgroundImage: `url("${image}")`
       } : {}
       const classNames = classnames({
         image,
