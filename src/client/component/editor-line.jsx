1cafc0e59f5c470ffda4034f2dd6748d430bafd9
Sho Hashimoto <hashimoto@shokai.org>
2016-06-12 01:29:03 +0900

    split editor-line.render()

--- src/client/component/editor-line.jsx
+++ src/client/component/editor-line.jsx
@@ -27,61 +27,76 @@ export default class EditorLine extends Component{
   }
 
   render(){
-    const {line, compiler, key} = this.props
-    if(this.props.edit){
-      let input = (
-        <input
-           ref="input"
-           value={line.value}
-           onChange={e => this.props.onChange(e.target.value)}
-           onClick={e => e.stopPropagation()}
-           onKeyDown={this.props.onKeyDown}
-           onPasteCapture={this.props.onPaste}
-          />
-      )
-      if(line.blocktitle && !line.codeblock && !line.cli) input = <h3>{input}</h3>
+    const {line, edit} = this.props
+    const {codeblock, cli} = line
+    if(edit) return this.renderEdit()
+    if(codeblock) return this.renderCodeBlock()
+    if(cli) return this.renderCli()
+    return this.renderDefault()
+  }
+
+  renderEdit(){
+    const {line, key} = this.props
+    let input = (
+      <input
+         ref="input"
+         value={line.value}
+         onChange={e => this.props.onChange(e.target.value)}
+        onClick={e => e.stopPropagation()}
+        onKeyDown={this.props.onKeyDown}
+        onPasteCapture={this.props.onPaste}
+        />
+    )
+    if(line.blocktitle && !line.codeblock && !line.cli) input = <h3>{input}</h3>
       return <li key={key} style={{marginLeft: line.indent*20}}>{input}</li>
-    }
-    if(line.codeblock){
-      let {lang, start, filename, indent} = line.codeblock
-      if(start){
-        return (
-          <li key={key} style={{marginLeft: line.indent*20}}>
-            <LongPress onLongPress={this.props.onStartEdit}>
-              <span className="codeblock-start">{filename || getFullLanguage(lang) || lang}</span>
-            </LongPress>
-          </li>
-        )
-      }
-      else{
-        let className = classnames({
-          codeblock: !line.codeblock.end,
-          "codeblock-end": line.codeblock.end
-        })
-        return (
-          <li key={key}>
-            <LongPress onLongPress={this.props.onStartEdit}>
-              <span className={className} style={{marginLeft: indent*20-5, paddingLeft: (line.indent-indent)*20+5}}>
-                <Code lang={lang} code={line.value} />
-              </span>
-            </LongPress>
-          </li>
-        )
-      }
-    }
-    if(line.cli){
+  }
+
+  renderCodeBlock(){
+    const {line, key} = this.props
+    const {lang, start, filename, indent} = line.codeblock
+    if(start){
       return (
         <li key={key} style={{marginLeft: line.indent*20}}>
           <LongPress onLongPress={this.props.onStartEdit}>
-            <span className="cli">
-              <span className="prefix">{line.cli.prefix}</span>
-              {" "}
-              <span>{line.cli.command}</span>
+            <span className="codeblock-start">{filename || getFullLanguage(lang) || lang}</span>
+          </LongPress>
+        </li>
+      )
+    }
+    else{
+      let className = classnames({
+        codeblock: !line.codeblock.end,
+        "codeblock-end": line.codeblock.end
+      })
+      return (
+        <li key={key}>
+          <LongPress onLongPress={this.props.onStartEdit}>
+            <span className={className} style={{marginLeft: indent*20-5, paddingLeft: (line.indent-indent)*20+5}}>
+              <Code lang={lang} code={line.value} />
             </span>
           </LongPress>
         </li>
       )
     }
+  }
+
+  renderCli(){
+    const {key, line} = this.props
+    return (
+      <li key={key} style={{marginLeft: line.indent*20}}>
+        <LongPress onLongPress={this.props.onStartEdit}>
+          <span className="cli">
+            <span className="prefix">{line.cli.prefix}</span>
+            {" "}
+            <span>{line.cli.command}</span>
+          </span>
+        </LongPress>
+      </li>
+    )
+  }
+
+  renderDefault(){
+    const {key, line, compiler} = this.props
     const icon = line.showUserIcon ? <UserIcon id={line.user} size={20} /> : null
     let value = line.value
     if(line.numberList) value = line.numberList.prefix + value
