61527cbb61f89942ae2c9156569a956c97f754b4
Sho Hashimoto <shokai@users.noreply.github.com>
2016-09-14 20:31:22 +0900

    include page title in text api (#81)

--- src/server/route/text.js
+++ src/server/route/text.js
@@ -8,9 +8,15 @@ const Page = mongoose.model("Page")
 router.get("/text/:wiki/:title", async (ctx, next) => {
   const {wiki, title} = ctx.params
   const page = await Page.findOneByWikiTitle({wiki, title})
-  if (!page) return ctx.status = 404
-  ctx.body = page.lines
-    .map(line => ' '.repeat(line.indent) + line.value)
-    .join('\n')
+  if (!page) {
+    ctx.status = 404
+    ctx.body = ''
+    return
+  }
+  ctx.body = [page.title]
+          .concat(
+            page.lines
+              .map(line => ' '.repeat(line.indent) + line.value)
+          ).join('\n')
   return
 })
